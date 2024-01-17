82ccc45bf29da852221b33d3a6db09b3ef031638
Carlos Daboin <carlos.daboin@ukg.com>
2024-01-17 07:39:01 -0500

    search and create APIs

---
+++ embedded_model.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class embedded_request(BaseModel):
+    SR: str = ''
+    Title: str = ''
+    Customer: str = ''
+    Description: str = ''
+    #sr_embedding: Optional[List[float]]
+    #title_embedding: Optional[List[float]]
+    #customer_embedding: Optional[List[float]]
+    #description_embedding: Optional[List[float]]
+
+class request_search(BaseModel):
+    SR: str = ''
+    Title: str = ''
+    Customer: str = ''
+    Description: str = ''
+    Score: float = 0.0
